8b90655ac5028615456b74602c0347181dd6f4b1
1025Rathod <1025rathod@gmail.com>
2020-03-16 23:53:38 +0530

    new

---
+++ assets/js/custom.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+     let siteInfo = $("#siteInfo").DataTable({
+          ordering: false,
+          searching: false,
+          lengthChange: false,
+          bInfo : false
+     });
+
+
+
+     function formatInvoice(d) {
+          // `d` is the original data object for the row
+          return `
+         <table class="table" id="siteInfoExp" width="100%">
+          <thead>
+               <tr>
+                    <th><input type="checkbox" onclick="checkAll(this)"></th>
+                    <th>Contact Name</th>
+                    <th>Contact Email</th>
+                    <th>Contact Number</th>
+                    <th>Business Purpose</th>
+               </tr>
+          </thead>
+          <tbody>
+               <tr>
+                    <td><input type="checkbox" onclick="checkAll(this)"></td>
+                    <td>Rakesh Kumar</td>
+                    <td>Rakesh.kdjfsh@gdlk.com</td>
+                    <td>+1 876 54673 876</td>
+                    <td>-</td>
+               </tr>
+               <tr>
+                    <td><input type="checkbox" onclick="checkAll(this)"></td>
+                    <td>Rakesh Kumar</td>
+                    <td>Rakesh.kdjfsh@gdlk.com</td>
+                    <td>+1 876 54673 876</td>
+                    <td>-</td>
+               </tr>
+          </tbody>
+     </table>
+     <div class="pagination">
+         <div class="col-md-6 text-left">
+             show <input type="text" class="pgnIpt" /> of 10
+         </div>
+         <div class="col-md-6 text-right">
+             <a href="#">
+                 <div class="float-right pgnBtn">Next
+                     <i class="fa fa-angle-right pgnicn" aria-hidden="true"></i>
+                 </div>
+             </a>
+             <a href="#"></a>
+             <div class="float-right pgnBtn blurBtn">
+                 <i class="fa fa-angle-left pgnicn" aria-hidden="true"></i>
+                 Previous
+             </div></a>
+         </div>
+     </div>
+             `;
+     }
+
+
+     $('#siteInfo tbody td:not(:first-child)').click(function () {
+          let tr = $(this).closest('tr');
+          let row = siteInfo.row(tr);
+
+          if (row.child.isShown()) {
+               // This row is already open - close it
+               row.child.hide();
+               tr.removeClass('shown');
+          }
+          else {
+               // Open this row
+               row.child(formatInvoice(row.data())).show();
+               tr.addClass('shown');
+          }
+     });
+});
---
+++ assets/js/datatables.init.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+    let invoiceTable = $("#datatable").DataTable();
+        $("#datatable-buttons").DataTable({
+            lengthChange: !1,
+            buttons: ["copy", "excel", "pdf", "colvis"]
+        }).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)");
+
+
+
+    function formatInvoice(d) {
+        // `d` is the original data object for the row
+        return `
+        <h5>Invoice Details</h5>
+        <hr>
+        <div class='row'>
+        
+            <div class='dtl_div col-md-3'>
+        
+                <div clas='order_details_show'>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">Transaction Source Type : BRM, Credit, Rebill </span> </p>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">Invoice Delivery Method : Post, Email </span> </p>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">Base Product: Hardware, Software, Licence</span> </p>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">Transaction Payment Terms : </span> </p>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">Legal Entity:</span> </p>
+                </div>
+        
+            </div>
+        
+            <div class='dtl_div col-md-2'>
+        
+                <div clas='order_details_show'>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">Sales Rep : </span> </p>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">End User Contact Information : </span> </p>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">End user name :</span> </p>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">End user Number :</span> </p>
+                </div>
+        
+            </div>
+        
+            <div class='dtl_div col-md-2'>
+        
+                <div clas='order_details_show'>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">Bill To Location</span> </p>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">Postfach 51 01 53,<br>Hannover, 30631,Germany <br>(Hannover-BILL_TO-101811577)</span> </p>
+        
+                </div>
+        
+            </div>
+        
+        
+            <div class='dtl_div col-md-2'>
+        
+                <div clas='order_details_show'>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">Bill To Site No</span> </p>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">Postfach 51 01 53,<br> Hannover, 30631, <br>Germany (Hannover-BILL_TO-101811577)</span> </p>
+        
+                </div>
+        
+            </div>
+        
+        
+            <div class='dtl_div col-md-2'>
+        
+                <div clas='order_details_show'>
+                    <p class="small font-weight-bolder text-break"><span class="small font-weight-bolder text-break">Unpaid Reason Code : </span> </p>
+                    <label>Last Transaction Level Notes</label><br> <input type="text"><br>
+                </div>
+        
+            </div>
+        
+        </div>
+            `;
+    }
+
+
+    $('#datatable tbody td:not(:first-child)').click(function () {
+        let tr = $(this).closest('tr');
+        let row = invoiceTable.row(tr);
+
+        if (row.child.isShown()) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            // Open this row
+            row.child(formatInvoice(row.data())).show();
+            tr.addClass('shown');
+        }
+    });
+});
